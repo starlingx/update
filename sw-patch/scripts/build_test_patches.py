1754d86f5cc9e6a7774294c85832364c3406da25
Luis Sampaio <luis.sampaio@windriver.com>
2022-12-01 14:28:49 -0800

    Update test patches to use LAT
    This commit updates the build_test_patches
    script to use the LAT container and the new
    patch process that has the option to reuse
    the initramfs.
    
    It also adds a --kernel option which rebuilds
    the kernel and creates a patch.
    
    Test Plan:
    Pass: ./build_test_patches.py --software-version \
    "${SW_VERSION}" --inservice --requires
    Pass: ./build_test_patches.py --software-version \
    "${SW_VERSION}" --type kernel
    Pass: build patches
    Pass: validate metadata content
    
    Story: 2009969
    Task: 45635
    Signed-off-by: Luis Sampaio <luis.sampaio@windriver.com>
    Change-Id: Idcd215b826844177b855d665f39da3351d229a13

--- sw-patch/scripts/build_test_patches.py
+++ sw-patch/scripts/build_test_patches.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 #
 # Copyright (c) 2022 Wind River Systems, Inc.
 #
@@ -7,35 +7,42 @@
 """
 Debian Build Test Patches:
 
-PATCH A) Reboot required - all nodes
-    Update package - logmgmt
-    rebuild the pkg
-    build-image to generate a new commit in the build ostree_repo
-    build a patch
+Default option (--default) builds 3 patches using the logmgmt package:
+    PATCH A) Reboot required - all nodes
+        Update package - logmgmt
+        rebuild the pkg
+        build-image to generate a new commit in the build ostree_repo
+        build a patch
 
-PATCH B) In Service patch
-    Update the metadata
-    Uses the example-restart script
-    Uses the same ostree commit as PATCH A so they can't be applied together
-    build a patch
+    PATCH B) In Service patch
+        Update the metadata
+        Uses the example-restart script
+        Uses the same ostree commit as PATCH A so they can't be applied together
+        build a patch
 
-PATCH C) Patch with dependency (reboot required, depends on PATCH A)
-    build PATCH A
-    update package - logmgmt
-    build-image to generate a new commit in the build ostree_repo
-    build Patch C (requires A)
+    PATCH C) Patch with dependency (reboot required, depends on PATCH A)
+        build PATCH A
+        update package - logmgmt
+        build-image to generate a new commit in the build ostree_repo
+        build Patch C (requires A)
 
-Requires:
-    debchange (devscripts) - Tool for maintenance of the debian/changelog file in a source package
-    https://manpages.debian.org/jessie/devscripts/debchange.1.en.html
+Kernel option (--kernel) builds 1 patch after rebuilding the kernel:
+    PATCH D) Reboot required - all nodes
+        update kernel-std and kernel-rt
+        rebuild the packages linux and linux-rt
+        build-image to generate a new commit in the build ostree_repo
+        build a patch with new initramfs
+        build a patch reusing initramfs
 
 Steps to run:
     # Setup debian build env
     # For more information about how to setup the environment:
         https://wiki.openstack.org/wiki/StarlingX/DebianBuildEnvironment
 
+    # Sample variables
     export PROJECT="stx-debian-build"
     export STX_BUILD_HOME="/localdisk/designer/${USER}/${PROJECT}"
+    export MY_REPO="/localdisk/designer/${USER}/${PROJECT}/repo/cgcs-root
     # Initialize the build containers
     stx control start
     ./build_test_patches.py --help
@@ -47,8 +54,8 @@ import os
 import shutil
 import subprocess
 import sys
-
-from requests import patch
+import yaml
+import xml.etree.ElementTree as ET
 
 sys.path.insert(0, "../cgcs-patch")
 from cgcs_make_patch.make_patch import PatchBuilder  # noqa: E402 pylint: disable=wrong-import-position
@@ -60,6 +67,9 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 log = logging.getLogger('build_test_patches')
+
+SAMPLE_RR_XML = "patch_recipe_rr_sample.xml"
+SAMPLE_INSVC_XML = "patch_recipe_insvc_sample.xml"
 
 
 def run_cmd(cmd):
@@ -91,44 +101,58 @@ class TestPatchBuilder():
         try:
             self.project = os.environ.get("PROJECT")
             self.build_home = os.environ.get("STX_BUILD_HOME")
-            self.repo = os.path.join(self.build_home, "repo")
-            self.repo_root = os.path.join(self.repo, "cgcs-root")
-            self.patch_repo_base = os.path.join(self.repo_root, "stx/update")
+            self.deploy_dir = os.path.join(os.environ.get("STX_BUILD_HOME"), "localdisk", "deploy")
+            self.repo_root = os.environ.get("MY_REPO")
+            self.stx_tools = os.path.join(self.repo_root, "..", "stx-tools")
+            self.patch_repo_base = os.path.join(self.repo_root, "stx", "update")
+            self.patch_tools_dir = os.path.join(self.patch_repo_base, "sw-patch", "cgcs-patch", "cgcs_make_patch")
             self.sw_version = sw_version
-            self.restart_script = os.path.join(self.patch_repo_base, "patch-scripts/EXAMPLE_0001/scripts/example-restart")
+            self.restart_script_path = os.path.join(self.patch_repo_base, "patch-scripts/EXAMPLE_0001/scripts")
+            self.restart_script_name = "example-restart"
         except TestPatchInitException:
             log.exception("TestPatchBuilder initialization failure")
             sys.exit(1)
 
     def __upversion_pkg(self, pkg_dir):
         """
-        Updates changelog version in pkg_dir/debian/deb_folder
+        Update package version using stx_patch
         """
         log.info("Upversioning package %s", pkg_dir)
-        # cd pkg_dir/debian/deb_folder; dch -q "PATCH TEST" --changelog changelog
         pwd = os.getcwd()
         os.chdir(pkg_dir)
-        # Increase the change log version
-        cmd = "dch -q 'PATCH TEST' --changelog debian/deb_folder/changelog"
-        ret = run_cmd(cmd)
-        if ret.returncode != 0:
-            raise Exception("Error while changing the package version")
+        meta_data_path = os.path.join(pkg_dir, "debian", "meta_data.yaml")
+        with open(meta_data_path) as f:
+            meta_data = yaml.safe_load(f)
+
+        if "stx_patch" in meta_data["revision"]:
+            meta_data["revision"]["stx_patch"] += 1
+        else:
+            meta_data["revision"]["stx_patch"] = 1
+
+        # Save updated meta_data.yaml
+        with open(meta_data_path, "w") as f:
+            yaml.dump(meta_data, f)
+
         os.chdir(pwd)
 
-    def __delete_dir(self, dir_path):
+    def delete_ostree_prepatch(self, dir_name):
         """
-        Deletes a directory - called when cleaning up the cloned ostree
-        param dir_path: Path to the directory
+        Deletes ostree_repo prepatch generated during prepare
         """
-        if os.path.isdir(dir_path):
-            log.info("removing %s", dir_path)
-            shutil.rmtree(dir_path)
+        cmd = f'''
+            source import-stx
+            stx shell --container lat -c \
+                "cd \\$DEPLOY_DIR; rm -rf {dir_name}"
+        '''
+        ret = run_cmd(cmd)
+        log.info("Clean up ostree prepatch directory returned %s", ret.returncode)
+        if ret.returncode != 0:
+            raise Exception("Failed to delete directory")
 
     def build_pkg(self, pkg_name):
         """
         Build package
         """
-        os.chdir(os.path.join(self.repo, "stx-tools"))
         cmd = f'''
             source import-stx
             stx shell -c "build-pkgs -c -p {pkg_name}"
@@ -151,7 +175,7 @@ class TestPatchBuilder():
         if ret.returncode != 0:
             raise Exception("Failed to build image")
 
-    def update_pkg(self, pname):
+    def update_logmgmt_pkg(self, pname):
         """
         Make a change on the logmgmt package and upversions it
         param pname: patch name that is added to the script and can be used as patch validation
@@ -167,6 +191,90 @@ class TestPatchBuilder():
         # build the pkg to apply the change
         self.build_pkg(pkg_name)
 
+    def prepare_env(self, ostree_clone_name):
+        """
+        Generates ostree_repo snapshot before creating a patch
+        It executes the command inside the LAT container
+        """
+        cmd = f'''
+            source import-stx
+            stx shell --container lat -c \
+                "cd \\$PATCH_TOOLS; python3 make_patch.py prepare --clone-repo {ostree_clone_name}"
+        '''
+        ret = run_cmd(cmd)
+        log.info("Patch prepare return code %s", ret.returncode)
+        if ret.returncode != 0:
+            raise Exception("Failed to run patch prepare")
+
+    def create_patch_xml(self, patch_id, sw_version, require_id, reboot=True):
+        """
+        Create patch xml at the patch_tools_dir
+        param patch_id: patch name/id
+        param sw_version: software version, e.g: 21.12
+        param required_id: patch id for prereq patch
+        param reboot: reboot required or insvc patch
+
+        return: file name
+        """
+        tree = ET.parse(SAMPLE_RR_XML) if reboot else ET.parse(SAMPLE_INSVC_XML)
+        metadata = tree.find("METADATA")
+        metadata.find("ID").text = patch_id
+        metadata.find("SW_VERSION").text = sw_version
+
+        if require_id:
+            requires_tag = metadata.find("REQUIRES")
+            reqid_tag = ET.SubElement(requires_tag, "ID")
+            reqid_tag.text = require_id
+
+        if not reboot:
+            #  update restart script path
+            metadata.find("RESTART_SCRIPT").text = os.path.join("/localdisk", "deploy", self.restart_script_name)
+
+        file_name = f"{patch_id}.xml"
+        tree.write(file_name)
+        return file_name
+
+    def make_patch_lat(self, xml_path, ostree_clone, formal=False, reuse_initramfs=True):
+        """
+        Calls the make_patch utility inside LAT container to generate our patch
+        param xml_path: path to the patch recipe/xml
+        param ostree_clone: path to the ostree_clone repo generated during the prepare stage
+        """
+        ostree_clone_lat = os.path.join("/localdisk", "deploy", ostree_clone)
+        delta_dir_lat = os.path.join("/localdisk", "deploy", "delta-dir")
+
+        if not reuse_initramfs:
+            reuse_env_var = "export NO_REUSE_INITRAMFS=True;"
+        else:
+            reuse_env_var = ""
+
+        cmd = f'''
+            source import-stx
+            stx shell --container lat -c "cd \\$PATCH_TOOLS; {reuse_env_var} python3 make_patch.py create \
+                --patch-recipe {xml_path} --clone-repo {ostree_clone_lat}/ \
+                    --delta-dir {delta_dir_lat}"
+        '''
+        if formal:
+            index = cmd.find("--clone-repo")
+            cmd = f"{cmd[:index]} --formal {cmd[index:]}"
+
+        ret = run_cmd(cmd)
+        log.info("Patch create return code %s", ret.returncode)
+        if ret.returncode != 0:
+            raise Exception("Failed to create patch")
+
+    def pull_local_patch_repo(self):
+        cmd = f'''
+            source import-stx
+            stx shell --container lat -c "cd \\$DEPLOY_DIR; \
+                ostree --repo=ostree_repo pull-local \
+                     {os.path.join('patch_work', 'patch_repo')} starlingx; \
+                        ostree --repo=ostree_repo summary --update"
+            '''
+        ret = run_cmd(cmd)
+        if ret.returncode != 0:
+            raise Exception("Failed to pull ostree from patch_repo process returned non-zero exit status %i", ret.returncode)
+
     def create_test_patches(self, pname, requires=False, inservice=False, formal=False):
         """
         Creates test patches:
@@ -177,90 +285,102 @@ class TestPatchBuilder():
         param formal: Signs the patch with formal key
         """
         ostree_clone_name = "ostree_repo_patch"
-        patch_builder = PatchBuilder()
+        os.chdir(self.stx_tools)
         # Generating ostree_repo clone
-        patch_builder.prepare_env(ostree_clone_name)
+        self.prepare_env(ostree_clone_name)
         # Update pkg
-        self.update_pkg(pname)
-        log.info("Generating RR patch for all nodes")
+        self.update_logmgmt_pkg(pname)
+        log.info("Generating Reboot required patch")
         # build image to trigger a new ostree commit
         self.build_image()
 
-        rr_patch_name = pname + "_RR_ALL_NODES"
+        os.chdir(self.patch_tools_dir)
         insvc_patch_name = pname + "_NRR_INSVC"
-        rr_requires_patch_name = pname + "_RR_ALL_NODES_REQUIRES"
+        rr_patch_name = pname + "_RR_ALL_NODES"
+        rr_req_patch_name = pname + "_RR_ALL_NODES_REQUIRES"
+        # Create patch xml files
+        insvc_xml_path = self.create_patch_xml(insvc_patch_name, self.sw_version, None, reboot=False)
+        rr_xml_path = self.create_patch_xml(rr_patch_name, self.sw_version, None)
+        rr_req_xml_path = self.create_patch_xml(rr_req_patch_name, self.sw_version, rr_patch_name)
 
-        patch_data = PatchRecipeData()
-        patch_data.patch_id = rr_patch_name
-        patch_data.sw_version = self.sw_version
-        patch_data.metadata = {
-            "SUMMARY": "RR ALL NODES",
-            "DESCRIPTION": "Test patch",
-            "INSTALL_INSTRUCTIONS": "Sample instructions",
-            "WARNINGS": "Sample Warning",
-            "STATUS": "DEV",
-            "UNREMOVABLE": "N",
-            "REBOOT_REQUIRED": "Y"
-        }
-        # Create a patch
-        log.info("Creating RR patch %s", patch_data.patch_id)
-        patch_builder.create_patch(patch_data, ostree_clone_name, formal)
-        log.info("RR Patch build done")
-
+        os.chdir(self.stx_tools)
+        # In service patch
         if inservice:
-            patch_data = PatchRecipeData()
-            patch_data.patch_id = insvc_patch_name
-            patch_data.sw_version = self.sw_version
-            patch_data.metadata = {
-                "SUMMARY": "IN SVC PATCH",
-                "DESCRIPTION": "Test In Service patch",
-                "INSTALL_INSTRUCTIONS": "Sample instructions",
-                "WARNINGS": "Sample Warning",
-                "STATUS": "DEV",
-                "UNREMOVABLE": "N",
-                "REBOOT_REQUIRED": "N"
-            }
-            patch_data.restart_script["full_path"] = self.restart_script
-            patch_data.restart_script["metadata_name"] = os.path.basename(self.restart_script)
-            log.info("Creating inservice patch %s", patch_data.patch_id)
-            log.info("restart script %s", patch_data.restart_script["full_path"])
-            patch_builder.create_patch(patch_data, ostree_clone_name, formal)
+            # build patch
+            log.info("Creating inservice patch %s", insvc_patch_name)
+            log.info("restart script %s/%s", self.restart_script_path, self.restart_script_name)
+            # Copy restart script to localdisk/deploy
+            shutil.copy2(os.path.join(self.restart_script_path, self.restart_script_name), self.deploy_dir)
+            # call lat
+            self.make_patch_lat(insvc_xml_path, ostree_clone_name, formal)
             log.info("Inservice patch build done")
 
-        clone_repo_path = os.path.join(patch_builder.deploy_dir, ostree_clone_name)
-        self.__delete_dir(clone_repo_path)
+        # RR Patch
+        log.info("Creating RR patch %s", rr_xml_path)
+        self.make_patch_lat(rr_xml_path, ostree_clone_name, formal)
+        log.info("RR Patch build done")
+
+        # Cleans up the ostree_clone to generate a new one for the requires patch
+        self.delete_ostree_prepatch(ostree_clone_name)
 
         if requires:
             # Build the 2nd patch which will follow similar steps but will set the requires flag
-            patch_builder.prepare_env(ostree_clone_name)
+            # If re-using initramfs it needs to pull the previous patch commit into ostree_repo
+            self.prepare_env(ostree_clone_name)
             # Update pkg
-            self.update_pkg(pname + "_REQUIRES")
+            self.update_logmgmt_pkg(rr_req_patch_name)
             # build image to trigger a new ostree commit
             self.build_image()
-            # Update patch ID and set requires
-            patch_data = PatchRecipeData()
-            patch_data.patch_id = rr_requires_patch_name
-            patch_data.sw_version = self.sw_version
-            patch_data.metadata = {
-                "SUMMARY": "RR ALL NODES REQUIRES",
-                "DESCRIPTION": "Test patch with dependency",
-                "INSTALL_INSTRUCTIONS": "Sample instructions",
-                "WARNINGS": "Sample Warning",
-                "STATUS": "DEV",
-                "UNREMOVABLE": "N",
-                "REBOOT_REQUIRED": "Y"
-            }
-            patch_data.requires.append(rr_patch_name)
             # Create a patch
-            log.info("Creating RR Requires patch %s", patch_data.patch_id)
-            patch_builder.create_patch(patch_data, ostree_clone_name, formal)
+            log.info("Creating RR Requires patch %s", rr_req_patch_name)
+            self.make_patch_lat(rr_req_xml_path, ostree_clone_name, formal)
             log.info("Requires patch build done")
-            self.__delete_dir(clone_repo_path)
+            self.delete_ostree_prepatch(ostree_clone_name)
+
+        # clean up restart script
+        os.remove(os.path.join(self.deploy_dir, self.restart_script_name))
+
+    def create_kernel_patch(self, sw_version, formal=False):
+        '''
+        Upversion and rebuilds the kernel
+        param sw_version: software version, e.g 22.12
+        '''
+        ostree_clone_name = "ostree_repo_patch"
+        os.chdir(self.patch_tools_dir)
+        # Create patch recipe/xml
+        patch_name = sw_version + "_KERNEL"
+        kernel_patch_xml = self.create_patch_xml(patch_name, self.sw_version, None)
+        kernel_patch_reuse_xml = self.create_patch_xml(patch_name + "_REUSE", self.sw_version, None)
+
+        os.chdir(self.stx_tools)
+        # Generating ostree_repo clone
+        self.prepare_env(ostree_clone_name)
+        # Update pkg
+        kernel_std_dir = os.path.join(self.repo_root, "stx/kernel", "kernel-std")
+        kernel_rt_dir = os.path.join(self.repo_root, "stx/kernel", "kernel-rt")
+        self.__upversion_pkg(kernel_std_dir)
+        self.__upversion_pkg(kernel_rt_dir)
+        log.info("Generating Kernel patch")
+        # Rebuild the kernel
+        self.build_pkg("linux")
+        self.build_pkg("linux-rt")
+        # build image to trigger a new ostree commit
+        self.build_image()
+
+        # Create a patch
+        log.info("Creating Kernel patch %s", kernel_patch_xml)
+        # Create patch without reusing the initramfs
+        self.make_patch_lat(kernel_patch_xml, ostree_clone_name, formal, reuse_initramfs=False)
+        # Create patch reusing initramfs
+        self.make_patch_lat(kernel_patch_reuse_xml, ostree_clone_name, formal)
+        log.info("Kernel patch build done")
+        self.delete_ostree_prepatch(ostree_clone_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Debian build_test_patches")
 
+    parser.add_argument("-t", "--type", default="default", type=str, help="Default (logmgmt patches) or kernel")
     parser.add_argument("-sw", "--software-version", type=str, help="Patch Software version, will prefix the patch name", default=None, required=True)
     parser.add_argument("-r", "--requires", action="store_true", help="Builds the 2nd patch which requires the rr_patch")
     parser.add_argument("-i", "--inservice", action="store_true", help="Builds the in service patch")
@@ -270,10 +390,13 @@ if __name__ == "__main__":
 
     try:
         log.info("Building test patches")
-        patch_name = args.software_version
+        sw_version = args.software_version
         test_patch_builder = TestPatchBuilder(args.software_version)
-        test_patch_builder.create_test_patches(patch_name, args.requires, args.inservice, args.formal)
-        log.info("Test patch build completed")
+        if args.type == "default":
+            test_patch_builder.create_test_patches(sw_version, args.requires, args.inservice, args.formal)
+        elif args.type == "kernel":
+            test_patch_builder.create_kernel_patch(sw_version, args.formal)
 
+        log.info("Test patch build completed")
     except TestPatchCreationException:
         log.exception("Error while creating test patches")
