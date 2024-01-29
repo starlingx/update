83d871a5da924dde9bb76f7f47298ba4fa5b27fb
Heitor Matsui <heitorvieira.matsui@windriver.com>
2024-01-29 14:53:34 -0300

    Include cleanup mechanism for deploy start
    This commit adds a cleanup script in both the success
    and failure cases for deploy start. The cleanup does:
    - Stop temporary database created for data migration
    - Unmount bind filesystems used by data migration
    - Removes temporary directories created by deploy start
    
    This commit also improves chroot_mounts.sh script, removing
    the duplicated code/filepaths, adding a function to check
    if mount points are mounted, simplifying the script logic
    and improving the messages returned to the user.
    
    Test Plan
    PASS: deploy start executed successfully, verify that cleanup
          was executed properly
    PASS: simulate failure scenarios during deploy start, verify that
          cleanup was executed properly
    PASS: manage to run deploy start successfully (reentrant) after
          the previous failure test case
    PASS: use deploy-cleanup script standalone to clean up the
          deploy start environment
    PASS: build iso with the commit to verify build process
    
    Story: 2010676
    Task: 49474
    
    Change-Id: I970e32f4a521427e5d56d5fb8e4865fdd9fa4bd8
    Signed-off-by: Heitor Matsui <heitorvieira.matsui@windriver.com>

--- software/scripts/chroot_mounts.sh
+++ software/scripts/chroot_mounts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 Wind River Systems, Inc.
+# Copyright (c) 2023-2024 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,16 +16,19 @@ ETC_PATH=/etc
 PROC_PATH=/proc
 LOG_PATH=/var/log
 
-mount_points=(
-    "$DEV_PATH"
-    "$PLATFORM_PATH"
-    "$RABBIT_PATH"
-    "$POSTGRES_PATH"
-    "$PROC_PATH"
-    "$LOG_PATH"
-)
-
 OSTREE_DEPLOYMENT_BRANCH="$1"
+
+# src:dst
+mount_points=(
+    "${DEV_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${DEV_PATH}"
+    "${PLATFORM_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${PLATFORM_PATH}"
+    "${RABBIT_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${RABBIT_PATH}"
+    "${POSTGRES_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${POSTGRES_PATH}"
+    "${PROC_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${PROC_PATH}"
+    "${LOG_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${LOG_PATH}"
+    "${OSTREE_DEPLOYMENT_BRANCH}/${USR_PATH}/${ETC_PATH}:${OSTREE_DEPLOYMENT_BRANCH}/${ETC_PATH}"
+    "${PLATFORM_CONF_PATH}:${TMP_PATH}/${PLATFORM_CONF_PATH}"
+)
 
 handle_error() {
     local exit_code="$1"
@@ -34,55 +37,87 @@ handle_error() {
     echo "Error: $error_message" >&2
     echo "Please check the error details and take appropriate action for recovery." >&2
 
+    # attempt to unmount if there were successful mounts before the error
+    umount_all
+
     exit "$exit_code"
 }
 
 mount_all() {
-    for dir in "${mount_points[@]}"; do
-        target_dir=$OSTREE_DEPLOYMENT_BRANCH$dir
-        if [ ! -d "$target_dir" ]; then
-            sudo mkdir -p "$target_dir"
-        fi
-        echo "mount --bind $dir $target_dir"
-        sudo mount --bind "$dir" "$target_dir" ||
-            handle_error 1 "Failed to bind mount $dir to $target_dir"
+    for mnt in ${mount_points[@]}; do
+        # split source and destination, squeeze multiple '/'
+        src_dst=($(echo ${mnt//:/ } | tr -s "/"))
+        src=${src_dst[0]}
+        dst=${src_dst[1]}
+
+        echo "mount --bind ${src} ${dst}"
+        sudo mkdir -p ${dst}
+        sudo mount --bind ${src} ${dst} || handle_error 1 "Failed to bind mount ${src} to ${dst}"
     done
-
-    if [ ! -d "$TMP_PATH$PLATFORM_CONF_PATH" ]; then
-        mkdir -p "$TMP_PATH$PLATFORM_CONF_PATH"
-    fi
-
-    echo "mkdir $OSTREE_DEPLOYMENT_BRANCH/etc -p"
-    sudo mkdir $OSTREE_DEPLOYMENT_BRANCH/etc -p
-    echo "mount --bind $OSTREE_DEPLOYMENT_BRANCH$USR_PATH$ETC_PATH $OSTREE_DEPLOYMENT_BRANCH$ETC_PATH"
-    sudo mount --bind "$OSTREE_DEPLOYMENT_BRANCH$USR_PATH$ETC_PATH" "$OSTREE_DEPLOYMENT_BRANCH$ETC_PATH" ||
-        handle_error 1 "Failed to bind mount $OSTREE_DEPLOYMENT_BRANCH$USR_PATH$ETC_PATH to
-        $OSTREE_DEPLOYMENT_BRANCH$ETC_PATH"
-
-    echo "mount --bind $PLATFORM_CONF_PATH $TMP_PATH$PLATFORM_CONF_PATH"
-    sudo mount --bind "$PLATFORM_CONF_PATH" "$TMP_PATH$PLATFORM_CONF_PATH" ||
-        handle_error 1 "Failed to bind mount $PLATFORM_CONF_PATH to $TMP_PATH$PLATFORM_CONF_PATH"
+    return 0
 }
 
 umount_all() {
-    for dir in "${mount_points[@]}"; do
-        target_dir=$OSTREE_DEPLOYMENT_BRANCH$dir
-        echo "sudo umount $target_dir"
-        sudo umount $target_dir
-    done
+    local rc=0
+    for mnt in ${mount_points[@]}; do
+        # split source and destination, squeeze multiple '/'
+        src_dst=($(echo ${mnt//:/ } | tr -s "/"))
+        src=${src_dst[0]}
+        dst=${src_dst[1]}
 
-    sudo umount $OSTREE_DEPLOYMENT_BRANCH$ETC_PATH
-    sudo umount $TMP_PATH$PLATFORM_CONF_PATH
+        echo "sudo umount $dst"
+        umount_output=$(sudo umount $dst 2>&1)
+        if [ $? -ne 0 ]; then
+            # ignore messages that are not harmful
+            if [[ ! $umount_output =~ ("not mounted"|"no mount point specified") ]]; then
+                echo $umount_output
+                rc=1
+            fi
+        fi
+    done
+    return $rc
 }
 
+check_all() {
+    local rc=0
+    local mounted=()
+    for mnt in ${mount_points[@]}; do
+        # split source and destination, squeeze multiple '/'
+        src_dst=($(echo ${mnt//:/ } | tr -s "/"))
+        src=${src_dst[0]}
+        dst=${src_dst[1]}
+
+        mount | grep -w $dst 2>&1 > /dev/null
+        if [[ $? -eq 0 ]]; then
+            rc=1
+            mounted+=(${dst})
+        fi
+    done
+    if [[ ${#mounted[@]} -gt 0 ]]; then
+        echo "Mounted mount points:"
+        for mnt in ${mounted[@]}; do
+            echo $mnt
+        done
+    fi
+    return $rc
+}
 
 if [ -z "$1" ]; then
-    echo "Error: Ostree deployment branch parameter is missing."
+    echo "Error: OSTree deployment branch parameter is missing."
     exit 1
 fi
 
-if [[ $# -eq 2 && $2 == "-u" ]]; then
-    umount_all
+if [[ $# -eq 2 ]]; then
+    if [[ $2 == "-u" ]]; then
+        umount_all
+        rc=$?
+    elif [[ $2 == "-c" ]]; then
+        check_all
+        rc=$?
+    fi
 else
     mount_all
+    rc=$?
 fi
+
+exit $rc
