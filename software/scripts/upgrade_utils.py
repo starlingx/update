208798a8567224293374c1c8806cbd21b2d196d4
Heitor Matsui <heitorvieira.matsui@windriver.com>
2024-02-08 17:30:36 -0300

    Update precheck URL with API version
    With commit [1] merged, precheck script now demands that the
    url used to access usm endpoints is updated with the API version.
    
    This commit updates the precheck URL.
    
    [1] https://review.opendev.org/c/starlingx/update/+/907197
    
    Test Plan
    PASS: run deploy-precheck successfully, verify that the health
          check for required patches is behaving as expected
    
    Story: 2010676
    Task: 49552
    
    Change-Id: I054871ce6d3968c0733d5234fe01b792fd1fd0d5
    Signed-off-by: Heitor Matsui <heitorvieira.matsui@windriver.com>

--- software/scripts/upgrade_utils.py
+++ software/scripts/upgrade_utils.py
@@ -50,7 +50,7 @@ def get_token_endpoint(config, service_type="platform"):
         raise Exception("Failed to get token and endpoint. Error: %s", str(e))
 
     if service_type == "usm":
-        endpoint += "/software"
+        endpoint += "/v1/software"
 
     return token, endpoint
 
