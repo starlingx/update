fdbb073719e0dd592c39cb092fd855b28fd34fc7
Luis Eduardo Bonatti <luizeduardo.bonatti@windriver.com>
2025-05-22 17:02:32 -0300

    USM: Change deploy precheck and start to accept arbitrary arguments.
    This change adds arbitrary parameters, to reduce or avoid the need to
    patchback when new features are added.
    The snapshot parameter was removed to use this new change with -o
    snapshot=true.
    This commit also add a sanitize function to allow only letters, numbers,
    space, - and _.
    Usage: software deploy start/precheck <major release> -o snapshot=true
    --options test=test
    
    Test Plan:
    PASS: Build-pkgs, Build-image, Install/Bootstrap/Unlock
    PASS: Check precheck script receives the parameters
    (by injecting logs manually)
    PASS: Check deploy start receives the parameters by injecting
    manual logs.
    PASS: Check software.json stores the parameters under options field.
    PASS: Deploy start blocked with reserved key in options.
    PASS: Check activate-rollback is using deploy options for snapshot.
    PASS: Major release deployment stx10 -> stx11 in sx.
    
    Story: 2011357
    Task: 52264
    
    Change-Id: I53b8863ceeec74c45831d959fe6cea00990cc156
    Signed-off-by: Luis Eduardo Bonatti <luizeduardo.bonatti@windriver.com>

--- software/software/api/controllers/v1/deploy.py
+++ software/software/api/controllers/v1/deploy.py
@@ -72,26 +72,24 @@ class DeployController(RestController):
         return result
 
     @expose(method='POST', template='json')
-    def precheck(self, release, force=None, region_name=None, snapshot=None):
+    def precheck(self, release, force=None, region_name=None, **kwargs):
         _force = force is not None
-        _snapshot = snapshot is not None
         reload_release_data()
 
-        result = sc.software_deploy_precheck_api(release, _force, region_name, _snapshot)
+        result = sc.software_deploy_precheck_api(release, _force, region_name, **kwargs)
         if result and 'error' in result and result["error"] != "":
             response.status = 406
         return result
 
     @expose(method='POST', template='json')
-    def start(self, release, force=None, snapshot=None):
+    def start(self, release, force=None, **kwargs):
         reload_release_data()
         _force = force is not None
-        _snapshot = snapshot is not None
 
         if sc.any_patch_host_installing():
             raise SoftwareServiceError(error="Rejected: One or more nodes are installing a release.")
 
-        result = sc.software_deploy_start_api(release, _force, _snapshot)
+        result = sc.software_deploy_start_api(release, _force, **kwargs)
 
         if result and 'error' in result and result["error"] != "":
             response.status = 406
